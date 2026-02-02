71804a8ae757bb5a1847141e188ae39fdf05501a
Marwen Boussabat <boussabatmarwen9@gmail.com>
2026-02-02 17:52:36 +0100

    start the first endpoint

---
+++ fastapi-ai/app/main.py
@@ -0,0 +1,13 @@
+import os 
+from fastapi import FastAPI
+from dotenv import load_dotenv
+import google.generativeai as genai
+
+load_dotenv()  # loads .env into environment variables
+google_api_key=os.getenv("Gemini_API_Key")
+
+app= FastAPI()
+
+@app.get("/")
+async def root():
+    return {"message":"API is running"}
